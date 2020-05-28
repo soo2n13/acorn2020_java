bead12345b7c40b92d7b1cc9b733fec178a0e36b
lee <soo2n13@naver.com>
2020-05-28 16:51:26 +0900

    MainClass08 added

---
+++ Step02_DataType/src/test/main/MainClass08.java
@@ -0,0 +1,13 @@
+package test.main;
+
+public class MainClass08 {
+	public static void main(String[] args) {
+		String str="abcde12345";
+		
+		int size=str.length();
+		
+		char ch=str.charAt(5);
+		
+		String result=str.toUpperCase();
+	}
+}
